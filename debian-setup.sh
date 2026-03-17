c689886b71f927da3508b24870a1fd777456bf8f
johanyim <johanjyyim@gmail.com>
2026-03-17 22:09:59 +0000

    debian setup

--- debian-setup.sh
+++ debian-setup.sh
@@ -9,17 +9,54 @@
 
 sudo apt install git neovim zsh zsh-syntax-highlighting ssh openssh-server man-db tmux firefox stow openssl pkgconf alacritty brightnessctl libnotify-bin clang npm unzip zoxide eza fonts-noto waybar starship fzf firefox stow
 
+# install rust (if it doesn't exist already)
+if ! [[ -x "$(command -v cargo)" ]]; then 
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi 
+
+
+
 # docker kubernetes
 
+# change default shell 
+if ! [[ $SHELL == "/usr/bin/zsh" ]]; then
+	chsh -s /usr/bin/zsh
+fi
+
+# start the ssh daemon
+sudo systemctl enable sshd
+sudo systemctl start sshd
 
 
-# setup noto fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/NotoSans.zip
-unzip NotoSans.zip -d NotoSans
-mkdir -p ~/.local/share/fonts
-cp ./NotoSans/*.ttf ~/.local/share/fonts/
+# setup ssh with passwordless login
+mkdir $HOME/.ssh
+touch $HOME/.ssh/authorized_keys
+printf "Linux:\nssh-copy-id $USER@$HOSTNAME\n\nWindows:\ntype C:\\\Users\\jyim\\.ssh\\id_ed25519.pub | ssh $USER@$HOSTNAME \"cat >> ~/.ssh/authorized_keys\"\n
+"
+
+# generate ssh public keys for git with ssh-keygen
+if ! [[ -f  "$HOME/.ssh/id_ed25519" ]]; then 
+	ssh-keygen -q -t ed25519 -N '' -f "$HOME/.ssh/id_ed25519"
+fi
+
+
+# setup fonts
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/DepartureMono.tar.xz
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Noto.tar.xz
+
+
+# unzip NotoSans.zip -d NotoSans
+# mkdir -p ~/.local/share/fonts
+# cp ./NotoSans/*.ttf ~/.local/share/fonts/
 fc-cache -fv
-rm -rf NotoSans
+
+
+
+
+# install rust (if it doesn't exist already)
+if ! [[ -x "$(command -v cargo)" ]]; then 
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi 
 
 
 
